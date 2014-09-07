fec5957bbf0ad336e7ca9191464f046f7f90e04c
Jayme Woogerd <jayme.woogerd@tufts.edu>
2014-09-07 18:56:02 -0400

    Populate list with suggestions, start event listener

--- vintage_spell.py
+++ vintage_spell.py
@@ -1,12 +1,11 @@
-import sublime_plugin 
-import sublime
+import sublime_plugin, sublime
 import hunspell 
 
 class VintageSpellCommand(sublime_plugin.TextCommand):
     def __init__(self, view):
         sublime_plugin.TextCommand.__init__(self, view)
         self.dictionary = hunspell.HunSpell('./en_US.dic', './en_US.aff')
-        # sublime.log_commands(True)
+        #sublime.log_commands(True)
 
     def run(self, edit, mode):
         region = self.view.sel()[0]
@@ -20,15 +19,30 @@ class VintageSpellCommand(sublime_plugin.TextCommand):
             if mode == 'replace_first':
                 self.replace(edit, word, suggestions[0])
             elif mode == 'show_list':
-                self.show_list(word, suggestions, self.view.window())
-            # tessst
-
+                suggestion = self.show_list(edit, text, suggestions)
+                self.replace(edit, word, suggestion)
+            # oneness
+            
     def replace(self, edit, word, suggestion):
         self.view.replace(edit, word, suggestion)
 
-    def show_list(self, word, suggestions, window):
-        list_view = window.new_file()
-        list_view.set_name('Change "%s" to:' % self.view.substr(word))
-        for suggestion in suggestions:
-            #region = sublime.Region(0, 10)
-            print suggestion
+    def show_list(self, edit, text, suggestions):
+        v = self.view.window().new_file()
+        v.set_name('Change "%s" to:' % text)
+        v.set_scratch(True)
+        v.settings().set('is_spell', True)
+
+        for i, suggestion in enumerate(suggestions):
+            v.insert(edit, v.text_point(i, 0), suggestion + '\n')
+
+        v.insert(edit, v.text_point(len(suggestions), 0), 
+            '\nType number and <Enter> (empty cancels): ')
+
+        return suggestions[0]
+
+
+class VintageSpellEventListener(sublime_plugin.EventListener):
+    def on_close(self, view):
+        if view.settings().get('is_spell') == True:
+            print 'close'
+
