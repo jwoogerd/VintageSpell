88b3dffe9c1450785370df2a8cab9968ab66d0ba
Jayme Woogerd <jayme.woogerd@tufts.edu>
2014-09-07 22:04:18 -0400

    Working implementation for first suggestion and list view

--- vintage_spell.py
+++ vintage_spell.py
@@ -2,47 +2,55 @@ import sublime_plugin, sublime
 import hunspell 
 
 class VintageSpellCommand(sublime_plugin.TextCommand):
+
     def __init__(self, view):
         sublime_plugin.TextCommand.__init__(self, view)
         self.dictionary = hunspell.HunSpell('./en_US.dic', './en_US.aff')
-        #sublime.log_commands(True)
+        self.selection = ''
+        self.word = None
+        self.suggestions = []
 
     def run(self, edit, mode):
         region = self.view.sel()[0]
-        word = self.view.word(region)
-        text = self.view.substr(word)
+        self.word = self.view.word(region)
+        text = self.view.substr(self.word)
 
         if self.dictionary.spell(text) == True:
             return
         else:
-            suggestions = self.dictionary.suggest(text)
+            self.suggestions = self.dictionary.suggest(text)
             if mode == 'replace_first':
-                self.replace(edit, word, suggestions[0])
+                self.selection = self.suggestions[0]
+                self.replace(edit)
             elif mode == 'show_list':
-                suggestion = self.show_list(edit, text, suggestions)
-                self.replace(edit, word, suggestion)
-            # oneness
-            
-    def replace(self, edit, word, suggestion):
-        self.view.replace(edit, word, suggestion)
+                self.show_list(text, self.suggestions)
 
-    def show_list(self, edit, text, suggestions):
+    def replace(self, edit):
+        self.view.replace(edit, self.word, self.selection)
+            
+    def show_list(self, text, suggestions):
         v = self.view.window().new_file()
         v.set_name('Change "%s" to:' % text)
         v.set_scratch(True)
-        v.settings().set('is_spell', True)
 
+        edit = v.begin_edit()
         for i, suggestion in enumerate(suggestions):
             v.insert(edit, v.text_point(i, 0), suggestion + '\n')
+        v.end_edit(edit)
 
-        v.insert(edit, v.text_point(len(suggestions), 0), 
-            '\nType number and <Enter> (empty cancels): ')
+        v.window().show_input_panel(
+            'Type number and <Enter> (empty cancels): ', '', 
+            self.on_done, None, self.on_cancel)
 
-        return suggestions[0]
-
-
-class VintageSpellEventListener(sublime_plugin.EventListener):
-    def on_close(self, view):
-        if view.settings().get('is_spell') == True:
-            print 'close'
-
+    def on_cancel(self):
+        sublime.active_window().run_command('close')
+        
+    def on_done(self, input):
+        try:
+            self.selection = self.suggestions[int(input) - 1]
+            edit = self.view.begin_edit()
+            self.replace(edit)
+            self.view.end_edit(edit)
+        except:
+            pass
+        sublime.active_window().run_command('close')
